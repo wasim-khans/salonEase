5cf222d7e1fa7e7ee0be8c13c0907d5fc5daf02e
wasim-khans <58463007+wasim-khans@users.noreply.github.com>
2026-03-23 03:56:55 +0000

    removing extra code

--- static/js/main.js
+++ static/js/main.js
@@ -1,22 +1,2 @@
 // SalonEase Main JavaScript
-// Basic client-side functionality
-
 console.log("Hey I am main.js and I will be used across all pages");
-
-// DOM Content Loaded
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('SalonEase application loaded');
-});
-
-// Basic utility functions
-window.SalonEase = {
-    formatDate: function(dateString) {
-        return new Date(dateString).toLocaleDateString();
-    },
-    formatTime: function(timeString) {
-        return timeString;
-    },
-    formatCurrency: function(amount) {
-        return '$' + amount;
-    }
-};
